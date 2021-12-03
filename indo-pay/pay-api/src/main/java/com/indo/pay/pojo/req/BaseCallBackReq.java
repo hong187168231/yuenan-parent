7e140804e7410b58d83b4c800708ea73f57e3b01
puff <yinpuff@gmail.com>
2021-12-03 15:23:16 +0800

    swagger

--- indo-pay/pay-api/src/main/java/com/indo/pay/pojo/req/BaseCallBackReq.java
+++ indo-pay/pay-api/src/main/java/com/indo/pay/pojo/req/BaseCallBackReq.java
@@ -12,11 +12,11 @@ import java.math.BigDecimal;
 public class BaseCallBackReq implements Serializable {
 
     private String notifyIp; // 回调ip
-    private String orderNo; //商户订单号(商户生成)
-    private String channelType; //支付通道
-    private BigDecimal amount; //实付金额
-    private String currency; //货币类型：CNY
+    private String mchId; //商户号
+    private String mchOrderNo; //商户订单号(商户生成)
+    private String oriAmount; //原始金额
+    private String amount; //实付金额
     private String transactionNo; //支付订单号(第三方支付生成)
-    private String status; //支付状态
+    private String status; //订单状态
 
 }
