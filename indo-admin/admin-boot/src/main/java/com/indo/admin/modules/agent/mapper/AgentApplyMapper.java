2dff90c02aa6d448c330775ee7447b938c6d5f25
puff <yinpuff@gmail.com>
2021-12-21 16:30:01 +0800

    1

---
+++ indo-admin/admin-boot/src/main/java/com/indo/admin/modules/agent/mapper/AgentApplyMapper.java
@@ -0,0 +1,18 @@
+package com.indo.admin.modules.agent.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.indo.admin.pojo.entity.AgentApply;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 会员下级表 Mapper 接口
+ * </p>
+ *
+ * @author puff
+ * @since 2021-10-23
+ */
+@Mapper
+public interface AgentApplyMapper extends BaseMapper<AgentApply> {
+
+}
