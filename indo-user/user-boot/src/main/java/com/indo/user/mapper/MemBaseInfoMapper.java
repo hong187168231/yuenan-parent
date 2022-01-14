21b216985d8bb0c905268101b7c6098197cf13ec
puff <yinpuff@gmail.com>
2022-01-14 21:52:17 +0800

    1

--- indo-user/user-boot/src/main/java/com/indo/user/mapper/MemBaseInfoMapper.java
+++ indo-user/user-boot/src/main/java/com/indo/user/mapper/MemBaseInfoMapper.java
@@ -2,7 +2,6 @@ package com.indo.user.mapper;
 
 import com.indo.core.base.mapper.SuperMapper;
 import com.indo.core.pojo.entity.MemBaseinfo;
-import com.indo.core.pojo.vo.MemTradingVO;
 import com.indo.user.pojo.bo.MemTradingBO;
 import org.apache.ibatis.annotations.Mapper;
 
