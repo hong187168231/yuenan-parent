0df4593f4f52b3b7147358ad5f5538255739c5fd
puff <yinpuff@gmail.com>
2022-01-22 15:33:36 +0800

    1

---
+++ indo-admin/admin-api/src/main/java/com/indo/admin/pojo/vo/agent/RebateStatVO.java
@@ -0,0 +1,33 @@
+package com.indo.admin.pojo.vo.agent;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@ApiModel
+public class RebateStatVO {
+
+    @ApiModelProperty(value = "我的佣金")
+    private BigDecimal rebateAmount;
+
+    @ApiModelProperty(value = "团队人数")
+    private Integer teamNum;
+
+    @ApiModelProperty(value = "团队充值")
+    private BigDecimal teamRecharge;
+
+    @ApiModelProperty(value = "团队投注")
+    private BigDecimal teamBet;
+
+    @ApiModelProperty(value = "日新增")
+    private Integer dayAddNum;
+
+    @ApiModelProperty(value = "日新增")
+    private Integer monthAddNum;
+
+
+
+}
