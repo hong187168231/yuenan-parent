1b4709cee540b1b05f11b2c3d1ebcc677a047009
puff <yinpuff@gmail.com>
2022-01-27 11:01:43 +0800

    1

--- indo-admin/admin-boot/src/main/java/com/indo/admin/modules/app/mapper/AppVersionMapper.java
+++ indo-admin/admin-boot/src/main/java/com/indo/admin/modules/app/mapper/AppVersionMapper.java
@@ -2,6 +2,7 @@ package com.indo.admin.modules.app.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.indo.core.pojo.entity.AppVersion;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.indo.core.pojo.entity.AppVersion;
  * @author xxx
  * @since 2022-01-25
  */
+@Mapper
 public interface AppVersionMapper extends BaseMapper<AppVersion> {
 
 }
