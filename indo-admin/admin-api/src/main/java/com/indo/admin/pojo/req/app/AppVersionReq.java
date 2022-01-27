06a0f1cadf3979eb74b7cb9b8a4c1b728c08849c
puff <yinpuff@gmail.com>
2022-01-27 11:10:24 +0800

    1

---
+++ indo-admin/admin-api/src/main/java/com/indo/admin/pojo/req/app/AppVersionReq.java
@@ -0,0 +1,53 @@
+package com.indo.admin.pojo.req.app;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.indo.common.base.BaseDTO;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Data
+public class AppVersionReq extends BaseDTO {
+
+
+    @ApiModelProperty(value = "版本id")
+    private Integer versionId;
+
+    @ApiModelProperty(value = "版本号")
+    private String version;
+
+    @ApiModelProperty(value = "版本名称")
+    private String versionName;
+
+    @ApiModelProperty(value = "更新类型, 0: 非弹框，1: 弹框，2: 强制")
+    private Integer updateType;
+
+    private Integer deviceType;
+
+    @ApiModelProperty(value = "下载地址")
+    private String downloadUrl;
+
+    @ApiModelProperty(value = "更新内容")
+    private String updateContent;
+
+    @ApiModelProperty(value = "状态 启用 - 禁用")
+    private Integer enable;
+
+    @ApiModelProperty(value = "发布时间")
+    private LocalDateTime releaseTime;
+
+    @ApiModelProperty(value = "创建人")
+    private String createUser;
+
+    @ApiModelProperty(value = "更新人")
+    private String updateUser;
+
+    @ApiModelProperty(value = "创建时间")
+    private String createTime;
+
+    @ApiModelProperty(value = "修改时间")
+    private String updateTime;
+
+}
