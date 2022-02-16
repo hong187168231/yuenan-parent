6839c8e235dded96ec07d0908170e88494c0ff21
puff <yinpuff@gmail.com>
2022-02-16 11:04:07 +0800

    1

---
+++ indo-user/user-boot/src/main/java/com/indo/user/mapper/AppVersionMapper.java
@@ -0,0 +1,18 @@
+package com.indo.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.indo.core.pojo.entity.AppVersion;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * app版本更新表 Mapper 接口
+ * </p>
+ *
+ * @author xxx
+ * @since 2022-01-25
+ */
+@Mapper
+public interface AppVersionMapper extends BaseMapper<AppVersion> {
+
+}
