50cb441003d26cdd792fdf69a2b4be4fd2e88d10
teman <xxx@gmail.com>
2022-03-26 20:19:58 +0800

    官方地址添加API注释

--- indo-admin/admin-boot/src/main/java/com/indo/admin/modules/msg/controller/MsgAddressController.java
+++ indo-admin/admin-boot/src/main/java/com/indo/admin/modules/msg/controller/MsgAddressController.java
@@ -6,7 +6,9 @@ import com.indo.admin.modules.msg.service.IMsgAddressService;
 import com.indo.admin.pojo.vo.msg.MsgAddressVO;
 import com.indo.common.result.Result;
 import com.indo.user.pojo.req.msg.MsgAddressQueryReq;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,10 @@ import java.util.List;
  * 消息官方地址
  * @author justin
  */
+@Api(tags = "官方地址")
 @RestController
 @RequestMapping("/api/v1/address")
+@Slf4j
 public class MsgAddressController {
 
 	@Autowired
