d0a7d632f25ed9ac00a1b62cec8b97846d117fda
jushi <jushi>
2022-03-26 17:23:46 +0800

    T9回调入口参数解析获取方式修改

--- indo-game/game-boot/src/main/java/com/indo/game/controller/t9/T9CallbackController.java
+++ indo-game/game-boot/src/main/java/com/indo/game/controller/t9/T9CallbackController.java
@@ -3,6 +3,7 @@ package com.indo.game.controller.t9;
 import com.indo.common.annotation.AllowAccess;
 import com.indo.common.config.OpenAPIProperties;
 import com.indo.common.utils.IPAddressUtil;
+import com.indo.game.common.util.PostRawParamsParseUtil;
 import com.indo.game.common.util.T9AESDecrypt;
 import com.indo.game.service.t9.T9CallbackService;
 import org.slf4j.Logger;
@@ -33,16 +34,19 @@ public class T9CallbackController {
     @RequestMapping(value = "/querypoint", method = RequestMethod.POST)
     @ResponseBody
     @AllowAccess
-    public Object callBackQuerypoint(String params, HttpServletRequest request) {
-        String ip = IPAddressUtil.getIpAddress(request);
-        logger.info("T9Callback callBackQuerypoint 回调,IP:{} params:{}", ip, params);
+    public Object callBackQuerypoint(HttpServletRequest request) {
+        
         String jsonStr = null;
         try {
+            String params = PostRawParamsParseUtil.getRequestPostBytes(request);
+            logger.info("T9Callback callBackQuerypoint 回调, requestparams:{}", params);
 //            params = "FKgMpX13N9XpRt89ZknMOa_T_u_LkqWO2bzTuiXz8QE";
             jsonStr = T9AESDecrypt.decrypt(params, OpenAPIProperties.T9_API_KEY, OpenAPIProperties.T9_API_IV);
         } catch (Exception e) {
-            logger.info("T9Callback {} callBackQuerypoint 解密失败", params, e);
+            logger.info("T9Callback {} callBackQuerypoint 解密失败", e);
         }
+        String ip = IPAddressUtil.getIpAddress(request);
+        logger.info("T9Callback callBackQuerypoint 回调,IP:{} decrypt.params:{}", ip, jsonStr);
         Object object = t9CallbackService.queryPoint(jsonStr, ip);
         logger.info("T9Callback callBackQuerypoint 回调返回数据, params:{}", object);
         return object;
@@ -54,16 +58,19 @@ public class T9CallbackController {
     @RequestMapping(value = "/withdrawal", method = RequestMethod.POST)
     @ResponseBody
     @AllowAccess
-    public Object callBackWithdrawal(String params, HttpServletRequest request) {
-        String ip = IPAddressUtil.getIpAddress(request);
-        logger.info("T9Callback callBackWithdrawal 回调,IP:{} params:{}", ip, params);
+    public Object callBackWithdrawal(HttpServletRequest request) {
+
         String jsonStr = null;
         try {
+            String params = PostRawParamsParseUtil.getRequestPostBytes(request);
+            logger.info("T9Callback callBackWithdrawal 回调, requestparams:{}", params);
 //            params = "Ym-gLO1IcHMJ9Y_NY2mguOqhjYxAly8StzVF0hCnz_BzIu1-bawAH2CFiZedrLrKW9iVg56Np2DwsYxmkxyLsHiUvFFC14JtfvcstBAiWhQ";
             jsonStr = T9AESDecrypt.decrypt(params, OpenAPIProperties.T9_API_KEY, OpenAPIProperties.T9_API_IV);
         } catch (Exception e) {
-            logger.info("T9Callback {} callBackWithdrawal 解密失败", params, e);
+            logger.info("T9Callback callBackWithdrawal 解密失败", e);
         }
+        String ip = IPAddressUtil.getIpAddress(request);
+        logger.info("T9Callback callBackWithdrawal 回调,IP:{} params:{}", ip, jsonStr);
         Object object = t9CallbackService.withdrawal(jsonStr, ip);
         logger.info("T9Callback callBackWithdrawal 回调返回数据, params:{}", object);
         return object;
@@ -75,16 +82,19 @@ public class T9CallbackController {
     @RequestMapping(value = "/deposit", method = RequestMethod.POST)
     @ResponseBody
     @AllowAccess
-    public Object callBackDeposit(String params, HttpServletRequest request) {
-        String ip = IPAddressUtil.getIpAddress(request);
-        logger.info("T9Callback callBackDeposit 回调,IP:{} params:{}", ip, params);
+    public Object callBackDeposit(HttpServletRequest request) {
+
         String jsonStr = null;
         try {
+            String params = PostRawParamsParseUtil.getRequestPostBytes(request);
+            logger.info("T9Callback callBackDeposit 回调, requestparams:{}", params);
 //            x = "Ym-gLO1IcHMJ9Y_NY2mguOqhjYxAly8StzVF0hCnz_CTlrzkDPAz0-zZBuTC0FZx448M_3IRdwnm23B2fHYLkYoP3ZjAbYKxLk003YdSiec";
             jsonStr = T9AESDecrypt.decrypt(params, OpenAPIProperties.T9_API_KEY, OpenAPIProperties.T9_API_IV);
         } catch (Exception e) {
-            logger.info("T9Callback {} callBackDeposit 解密失败", params, e);
+            logger.info("T9Callback callBackDeposit 解密失败", e);
         }
+        String ip = IPAddressUtil.getIpAddress(request);
+        logger.info("T9Callback callBackDeposit 回调,IP:{} params:{}", ip, jsonStr);
         Object object = t9CallbackService.deposit(jsonStr, ip);
         logger.info("T9Callback callBackDeposit 回调返回数据, params:{}", object);
         return object;
@@ -96,16 +106,19 @@ public class T9CallbackController {
     @RequestMapping(value = "/canceltransfer", method = RequestMethod.POST)
     @ResponseBody
     @AllowAccess
-    public Object callBackCanceltransfer(String params, HttpServletRequest request) {
-        String ip = IPAddressUtil.getIpAddress(request);
-        logger.info("T9Callback callBackCanceltransfer 回调,IP:{} params:{}", ip, params);
+    public Object callBackCanceltransfer(HttpServletRequest request) {
+
         String jsonStr = null;
         try {
+            String params = PostRawParamsParseUtil.getRequestPostBytes(request);
+            logger.info("T9Callback callBackCanceltransfer 回调, requestparams:{}", params);
 //            x = "Ym-gLO1IcHMJ9Y_NY2mguOqhjYxAly8StzVF0hCnz_ACi8PVpFdETLicyN6fSbdk";
             jsonStr = T9AESDecrypt.decrypt(params, OpenAPIProperties.T9_API_KEY, OpenAPIProperties.T9_API_IV);
         } catch (Exception e) {
-            logger.info("T9Callback {} callBackCanceltransfer 解密失败", params, e);
+            logger.info("T9Callback callBackCanceltransfer 解密失败", e);
         }
+        String ip = IPAddressUtil.getIpAddress(request);
+        logger.info("T9Callback callBackCanceltransfer 回调,IP:{} params:{}", ip, jsonStr);
         Object object = t9CallbackService.canceltransfer(jsonStr, ip);
         logger.info("T9Callback callBackCanceltransfer 回调返回数据, params:{}", object);
         return object;
