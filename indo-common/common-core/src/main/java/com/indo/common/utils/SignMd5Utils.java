92c6fecb74de6fbb210510f4a80ab46a0fffb227
yangchen <yangchen>
2022-04-15 15:19:09 +0700

    BL

--- indo-common/common-core/src/main/java/com/indo/common/utils/SignMd5Utils.java
+++ indo-common/common-core/src/main/java/com/indo/common/utils/SignMd5Utils.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import sun.misc.BASE64Encoder;
 
 import java.security.KeyFactory;
+import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
@@ -193,6 +194,27 @@ public class SignMd5Utils {
             e.printStackTrace();
         }
         return null;
+    }
+    public static String getSha1(String str) {
+
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f' };
+        try {
+            MessageDigest mdTemp = MessageDigest.getInstance("SHA1");
+            mdTemp.update(str.getBytes("UTF-8"));
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char buf[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                buf[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                buf[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(buf);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     /**
