a965cc8db17ef422767d19b6f10bc53d9866cd43
huohu <huohu>
2022-04-19 22:16:41 +0800

    WM参数接收方式修改

--- indo-game/game-boot/src/main/java/com/indo/game/controller/wm/WmCallbackController.java
+++ indo-game/game-boot/src/main/java/com/indo/game/controller/wm/WmCallbackController.java
@@ -4,6 +4,7 @@ package com.indo.game.controller.wm;
 import com.alibaba.fastjson.JSONObject;
 import com.indo.common.annotation.AllowAccess;
 import com.indo.common.utils.IPAddressUtil;
+import com.indo.game.common.util.PostRawParamsParseUtil;
 import com.indo.game.service.wm.WmCallbackService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,7 +27,17 @@ public class WmCallbackController {
 
     @RequestMapping(value = "/callBack", method = RequestMethod.POST)
     @AllowAccess
-    public Object callBack(@RequestBody JSONObject params, HttpServletRequest request) {
+    public Object callBack(HttpServletRequest request) {
+        JSONObject params = new JSONObject();
+        try {
+            String[] datas = PostRawParamsParseUtil.getRequestPostBytes(request).split("&");
+            for (String data : datas) {
+                params.put(data.split("=")[0], data.split("=")[1]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            params = new JSONObject();
+        }
         logger.info("wmCallback callBack 回调请求, params:{}", params);
         // 查询余额
         if ("CallBalance".equals(params.getString("cmd"))) {
