463676f39fb3c72e15431ac449ac1891b8a7fdf2
一休 <hongchengwei1986@gmail.com>
2022-06-08 19:04:55 +0800

    20220608----yixiu---------null

--- indo-game/game-boot/src/main/java/com/indo/game/controller/app/GameController.java
+++ indo-game/game-boot/src/main/java/com/indo/game/controller/app/GameController.java
@@ -148,7 +148,7 @@ public class GameController {
         log.info("登录平台或单一游戏登录 进入游戏。。。 platform:{},loginUser:{},parentName:{}", platform, loginUser, parentName);
         //黑名单校验
         List<SysIpLimit> list =sysIpLimitClient.findSysIpLimitByType(1).getData();
-        if(list!=null||list.size()>0){
+        if(list!=null&&list.size()>0){
             // 获取请求信息
             String clientIP = IPUtils.getIpAddr(request);
             Boolean status = false;
