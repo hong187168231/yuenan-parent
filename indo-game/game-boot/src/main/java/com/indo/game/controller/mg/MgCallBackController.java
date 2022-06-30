13276bc3c29c3b0f7403641867b99400f10c3983
一休 <hongchengwei1986@gmail.com>
2022-06-30 19:36:09 +0800

    20220630----yixiu---------游戏api登录

--- indo-game/game-boot/src/main/java/com/indo/game/controller/mg/MgCallBackController.java
+++ indo-game/game-boot/src/main/java/com/indo/game/controller/mg/MgCallBackController.java
@@ -1,6 +1,7 @@
 package com.indo.game.controller.mg;
 
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
 import com.indo.common.annotation.AllowAccess;
 import com.indo.common.utils.IPAddressUtil;
 import com.indo.game.pojo.dto.mg.MgCallBackReq;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/mg/callBack")
@@ -30,10 +32,21 @@ public class MgCallBackController {
     @RequestMapping(value = "/getbalance", method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
     @ResponseBody
     @AllowAccess
-    public Object getBalance(@RequestBody MgCallBackReq mgCallBackReq, HttpServletRequest request) {
+    public Object getBalance(@RequestBody JSONObject jsonObject, HttpServletRequest request) {
 
         String ip = IPAddressUtil.getIpAddress(request);
-        logger.info("cqCallBack {} getBalance 回调,params:{}", JSONObject.toJSONString(mgCallBackReq));
+        logger.info("cqCallBack {} getBalance 回调,params:{}", JSONObject.toJSONString(jsonObject));
+        MgCallBackReq mgCallBackReq = new MgCallBackReq();
+        mgCallBackReq.setPlayerId(jsonObject.getString("playerId"));
+//        mgCallBackReq.setTxnType(jsonObject.getString("txnType"));
+//        mgCallBackReq.setTxnEventType(jsonObject.getString("txnEventType"));
+//        mgCallBackReq.setAmount(jsonObject.getBigDecimal("amount"));
+//        mgCallBackReq.setCurrency(jsonObject.getString("currency"));
+//        mgCallBackReq.setTxnId(jsonObject.getString("txnId"));
+//        mgCallBackReq.setCreationTime(jsonObject.getString("creationTime"));
+//        mgCallBackReq.setBetId(jsonObject.getString("betId"));
+//        mgCallBackReq.setRoundId(jsonObject.getString("roundId"));
+//        mgCallBackReq.setCompleted(jsonObject.getBoolean("completed"));
         Object object = mgCallbackService.mgBalanceCallback(mgCallBackReq, ip);
         logger.info("cqCallBack {} getBalance 回调下注返回数据 params:{}", object);
         return object;
@@ -45,10 +58,21 @@ public class MgCallBackController {
     @RequestMapping(value = "/login", method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
     @ResponseBody
     @AllowAccess
-    public Object verifySession(@RequestBody MgCallBackReq mgCallBackReq,HttpServletRequest request) {
+    public Object verifySession(@RequestBody JSONObject jsonObject,HttpServletRequest request) {
 
         String ip = IPAddressUtil.getIpAddress(request);
-        logger.info("pgCallBack {} verifySession回调,params:{}", JSONObject.toJSONString(mgCallBackReq));
+        logger.info("pgCallBack {} verifySession回调,params:{}", JSONObject.toJSONString(jsonObject));
+        MgCallBackReq mgCallBackReq = new MgCallBackReq();
+        mgCallBackReq.setPlayerId(jsonObject.getString("playerId"));
+        mgCallBackReq.setTxnType(jsonObject.getString("txnType"));
+        mgCallBackReq.setTxnEventType(jsonObject.getString("txnEventType"));
+        mgCallBackReq.setAmount(jsonObject.getBigDecimal("amount"));
+        mgCallBackReq.setCurrency(jsonObject.getString("currency"));
+        mgCallBackReq.setTxnId(jsonObject.getString("txnId"));
+        mgCallBackReq.setCreationTime(jsonObject.getString("creationTime"));
+        mgCallBackReq.setBetId(jsonObject.getString("betId"));
+        mgCallBackReq.setRoundId(jsonObject.getString("roundId"));
+        mgCallBackReq.setCompleted(jsonObject.getBoolean("completed"));
         Object object = mgCallbackService.mgVerifyCallback(mgCallBackReq, ip);
         logger.info("pgCallBack {} verifySession回调返回数据 params:{}", object);
         return object;
@@ -61,10 +85,21 @@ public class MgCallBackController {
     @RequestMapping(value = "/updatebalance", method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
     @ResponseBody
     @AllowAccess
-    public Object adjustment(@RequestBody MgCallBackReq mgCallBackReq,HttpServletRequest request) {
+    public Object adjustment(@RequestBody JSONObject jsonObject,HttpServletRequest request) {
 
         String ip = IPAddressUtil.getIpAddress(request);
-        logger.info("pgCallBack {} adjustment回调,params:{}", JSONObject.toJSONString(mgCallBackReq));
+        logger.info("pgCallBack {} adjustment回调,params:{}", JSONObject.toJSONString(jsonObject));
+        MgCallBackReq mgCallBackReq = new MgCallBackReq();
+        mgCallBackReq.setPlayerId(jsonObject.getString("playerId"));
+        mgCallBackReq.setTxnType(jsonObject.getString("txnType"));
+        mgCallBackReq.setTxnEventType(jsonObject.getString("txnEventType"));
+        mgCallBackReq.setAmount(jsonObject.getBigDecimal("amount"));
+        mgCallBackReq.setCurrency(jsonObject.getString("currency"));
+        mgCallBackReq.setTxnId(jsonObject.getString("txnId"));
+        mgCallBackReq.setCreationTime(jsonObject.getString("creationTime"));
+        mgCallBackReq.setBetId(jsonObject.getString("betId"));
+        mgCallBackReq.setRoundId(jsonObject.getString("roundId"));
+        mgCallBackReq.setCompleted(jsonObject.getBoolean("completed"));
         Object object = mgCallbackService.mgUpdatebalanceCallback(mgCallBackReq, ip);
         logger.info("pgCallBack {} adjustment回调返回数据 params:{}", object);
         return object;
