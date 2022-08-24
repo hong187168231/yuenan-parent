fec79a3ec98d92afa9f86ae9ceb7e3527eef25c0
louis <louis@qq.com>
2022-08-24 18:05:17 +0800

    打码量模块

--- indo-admin/admin-boot/src/main/java/com/indo/admin/modules/mem/mapper/MemBaseinfoMapper.java
+++ indo-admin/admin-boot/src/main/java/com/indo/admin/modules/mem/mapper/MemBaseinfoMapper.java
@@ -2,8 +2,10 @@ package com.indo.admin.modules.mem.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.indo.admin.pojo.dto.MemBetInfoDTO;
 import com.indo.admin.pojo.req.mem.MemBaseInfoReq;
 import com.indo.admin.pojo.vo.mem.MemBaseInfoVo;
+import com.indo.admin.pojo.vo.mem.MemBetInfoVo;
 import com.indo.core.pojo.bo.MemBaseInfoBO;
 import com.indo.core.pojo.entity.MemBaseinfo;
 import org.apache.ibatis.annotations.Mapper;
@@ -29,4 +31,6 @@ public interface MemBaseinfoMapper extends BaseMapper<MemBaseinfo> {
     MemBaseInfoBO findMemBaseInfoByAccount(@Param("account") String account);
 
     Page<MemBaseInfoBO> findIpRepeatAll(@Param("page") Page<MemBaseInfoBO> page,@Param("dto") MemBaseInfoReq dto);
+
+    Page<MemBetInfoVo> findMemBetInfo (@Param("page") Page<MemBetInfoVo> page,@Param("dto") MemBetInfoDTO dto);
 }
