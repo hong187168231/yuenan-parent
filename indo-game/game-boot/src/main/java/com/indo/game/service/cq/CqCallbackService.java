9394fb61cec2786fd2dd207337bd7bd215504fc7
一休 <hongchengwei1986@gmail.com>
2022-08-31 23:09:11 +0800

    20220831游戏回调调整

--- indo-game/game-boot/src/main/java/com/indo/game/service/cq/CqCallbackService.java
+++ indo-game/game-boot/src/main/java/com/indo/game/service/cq/CqCallbackService.java
@@ -1,9 +1,7 @@
 package com.indo.game.service.cq;
 
 
-import com.indo.game.pojo.dto.cq.CqBetCallBackReq;
-import com.indo.game.pojo.dto.cq.CqEndroundCallBackReq;
-import com.indo.game.pojo.dto.cq.CqEndroundDataCallBackReq;
+import com.indo.game.pojo.dto.cq.*;
 
 public interface CqCallbackService {
 
@@ -11,6 +9,10 @@ public interface CqCallbackService {
     Object cqBetCallback(CqBetCallBackReq cqApiRequestData, String ip, String wtoken);
 
     public Object endround(CqEndroundCallBackReq endroundDataCallBackReq, String ip, String wtoken);
+
+    public Object cqRolloutCallback(CqBetCallBackReq cqApiRequestData, String ip, String wtoken);
+
+    public Object cqTakeallCallback(CqBetCallBackReq cqApiRequestData, String ip, String wtoken);
 
     Object cqPayOffCallback(CqBetCallBackReq cqApiRequestData, String ip, String wtoken);
 
@@ -22,9 +24,23 @@ public interface CqCallbackService {
 
     Object cqRollinCallback(CqBetCallBackReq cqApiRequestData, String ip, String wtoken);
 
+    public Object cqRefundCallback(CqBetCallBackReq cqApiRequestData, String ip, String wtoken);
+
     Object cqBalanceCallback(String account, String ip, String wtoken);
 
     Object cqCheckPlayerCallback(String account, String ip, String wtoken);
 
     Object cqRecordCallback(String mtcode, String ip, String wtoken);
+
+    public Object cqBetsCallback(CqSportsCallBackReq<CqSportsInfoCallBackReq> cqSportsCallBackReq, String ip, String wtoken);
+
+    public Object cqRefundsCallback(CqSportsRefudsCallBackReq cqSportsRefudsCallBackReq, String ip, String wtoken);
+
+    public Object cqCancelCallback(CqSportsRefudsCallBackReq cqSportsRefudsCallBackReq, String ip, String wtoken);
+
+    public Object cqWinsCallback(CqSportsWinsCallBackReq<CqSportsEventCallBackReq> callBackReqCqSportsWinsCallBackReq, String ip, String wtoken);
+
+    public Object cqAmendCallback(CqSportsAmendCallBackReq<CqSportsAmendDataCallBackReq> cqSportsAmendCallBackReq, String ip, String wtoken);
+
+    public Object cqAmendsCallback(CqSportsAmendCallBackReq<CqSportsAmendDataCallBackReq> cqSportsAmendCallBackReq, String ip, String wtoken);
 }
