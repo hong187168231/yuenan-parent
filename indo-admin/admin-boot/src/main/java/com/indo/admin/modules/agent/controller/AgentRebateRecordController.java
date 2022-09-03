b99555c13bf0631902ebac77023d98cd6719fc64
louis <louis@qq.com>
2022-09-03 15:50:26 +0800

    修改BUG

--- indo-admin/admin-boot/src/main/java/com/indo/admin/modules/agent/controller/AgentRebateRecordController.java
+++ indo-admin/admin-boot/src/main/java/com/indo/admin/modules/agent/controller/AgentRebateRecordController.java
@@ -9,6 +9,7 @@ import com.indo.common.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class AgentRebateRecordController {
     private IAgentRebateRecordService iAgentRebateRecordService;
 
     @ApiOperation(value = "分页查询")
-    @PostMapping(value = "/page")
+    @GetMapping(value = "/page")
     public Result<List<AgentRebateRecordVO>> getPage(AgentRebateRecordReq req) {
         Page<AgentRebateRecordVO> result = iAgentRebateRecordService.queryList(req);
         return Result.success(result.getRecords(), result.getTotal());
