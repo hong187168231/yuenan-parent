dbd778d05105015ce713f66b5221b5cd2151ba44
一休 <hongchengwei1986@gmail.com>
2022-09-13 13:55:04 +0800

    20220913UG 游戏取消时候bug修复

--- indo-game/game-boot/src/main/java/com/indo/game/controller/ug/UgCallBackController.java
+++ indo-game/game-boot/src/main/java/com/indo/game/controller/ug/UgCallBackController.java
@@ -28,13 +28,13 @@ public class UgCallBackController {
     @ResponseBody
     @AllowAccess
     public Object checkLogin(@RequestBody JSONObject jsonObject) {
-        logger.info("ugCallBack callBack 回调玩家登入验证URL,checkLogin params:{}",JSONObject.toJSONString(jsonObject));
+        logger.info("ugCallBack checkLogin 回调玩家登入验证URL,checkLogin params:{}",JSONObject.toJSONString(jsonObject));
         UgCallBackCheckLoginReq ugCallBackCheckLoginReq = new UgCallBackCheckLoginReq();
         ugCallBackCheckLoginReq.setApiPassword(jsonObject.getString("apiPassword"));
         ugCallBackCheckLoginReq.setUserId(jsonObject.getString("userId"));
         ugCallBackCheckLoginReq.setToken(jsonObject.getString("token"));
         Object getBalance = ugCallbackService.checkLogin(ugCallBackCheckLoginReq);
-        logger.info("ugCallBackcallBack 回调玩家登入验证URL返回数据,checkLogin params:{}",JSONObject.toJSONString(getBalance));
+        logger.info("ugCallBack checkLogin 回调玩家登入验证URL返回数据,checkLogin params:{}",JSONObject.toJSONString(getBalance));
         return getBalance;
     }
     /**
@@ -44,12 +44,12 @@ public class UgCallBackController {
     @ResponseBody
     @AllowAccess
     public Object getBalance(@RequestBody JSONObject jsonObject) {
-        logger.info("ugCallBack callBack 回调,getBalance获取余额 params:{}",JSONObject.toJSONString(jsonObject));
+        logger.info("ugCallBack getBalance 回调,getBalance获取余额 params:{}",JSONObject.toJSONString(jsonObject));
         UgCallBackGetBalanceReq ugCallBackGetBalanceReq = new UgCallBackGetBalanceReq();
         ugCallBackGetBalanceReq.setApiPassword(jsonObject.getString("apiPassword"));
         ugCallBackGetBalanceReq.setUserId(jsonObject.getString("userId"));
         Object getBalance = ugCallbackService.getBalance(ugCallBackGetBalanceReq);
-        logger.info("ugCallBack  callBack 回调返回数据,getBalance获取余额 params:{}",JSONObject.toJSONString(getBalance));
+        logger.info("ugCallBack  getBalance 回调返回数据,getBalance获取余额 params:{}",JSONObject.toJSONString(getBalance));
         return getBalance;
     }
 
@@ -60,10 +60,10 @@ public class UgCallBackController {
     @ResponseBody
     @AllowAccess
     public Object transfer(@RequestBody JSONObject jsonObject) {
-        logger.info("ugCallBack  callBack 回调,transfer加余额/扣除余额 params:{}",JSONObject.toJSONString(jsonObject));
+        logger.info("ugCallBack  transfer 回调,transfer加余额/扣除余额 params:{}",JSONObject.toJSONString(jsonObject));
         UgCallBackTransferReq<UgCallBackTransactionItemReq> ugCallBackTransferReq = JSONObject.toJavaObject(jsonObject,UgCallBackTransferReq.class);
         Object transfer = ugCallbackService.transfer(ugCallBackTransferReq);
-        logger.info("ugCallBack callBack 回调返回数据,transfer加余额/扣除余额 params:{}",JSONObject.toJSONString(transfer));
+        logger.info("ugCallBack transfer 回调返回数据,transfer加余额/扣除余额 params:{}",JSONObject.toJSONString(transfer));
         return transfer;
     }
 
@@ -74,11 +74,11 @@ public class UgCallBackController {
     @ResponseBody
     @AllowAccess
     public Object cancel(@RequestBody JSONObject jsonObject) {
-        logger.info("ugCallBack  callBack 回调,cancel取消交易 params:{}",JSONObject.toJSONString(jsonObject));
+        logger.info("ugCallBack  cancel 回调,cancel取消交易 params:{}",JSONObject.toJSONString(jsonObject));
         UgCallBackCancelReq<UgCallBackCancelItemReq> ugCallBackCancelReq = JSONObject.toJavaObject(jsonObject,UgCallBackCancelReq.class);
 
         Object cancel = ugCallbackService.cancel(ugCallBackCancelReq);
-        logger.info("ugCallBack   callBack 回调返回数据,cancel取消交易 params:{}",JSONObject.toJSONString(cancel));
+        logger.info("ugCallBack   cancel 回调返回数据,cancel取消交易 params:{}",JSONObject.toJSONString(cancel));
         return cancel;
     }
 
@@ -89,10 +89,10 @@ public class UgCallBackController {
     @ResponseBody
     @AllowAccess
     public Object check(@RequestBody JSONObject jsonObject) {
-        logger.info("ugCallBack  callBack 回调,check检查交易结果 params:{}",JSONObject.toJSONString(jsonObject));
+        logger.info("ugCallBack  check 回调,check检查交易结果 params:{}",JSONObject.toJSONString(jsonObject));
         UgCallBackCheckTxnReq<UgCallBackCheckTxnItemReq> ugCallBackCheckTxnReq = JSONObject.toJavaObject(jsonObject,UgCallBackCheckTxnReq.class);
         Object check = ugCallbackService.check(ugCallBackCheckTxnReq);
-        logger.info("ugCallBack  callBack 回调返回数据,check检查交易结果 params:{}",JSONObject.toJSONString(check));
+        logger.info("ugCallBack  check 回调返回数据,check检查交易结果 params:{}",JSONObject.toJSONString(check));
         return check;
     }
 }
