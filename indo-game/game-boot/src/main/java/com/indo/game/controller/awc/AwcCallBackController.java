0278131e60772f8ce3c105066aaf14cafa884c38
一休 <hongchengwei1986@gmail.com>
2022-09-21 16:22:49 +0800

    20220921AE真人游戏BUG修复

--- indo-game/game-boot/src/main/java/com/indo/game/controller/awc/AwcCallBackController.java
+++ indo-game/game-boot/src/main/java/com/indo/game/controller/awc/AwcCallBackController.java
@@ -2,6 +2,7 @@ package com.indo.game.controller.awc;
 
 import com.alibaba.fastjson.JSONObject;
 import com.indo.common.annotation.AllowAccess;
+import com.indo.common.utils.DateUtils;
 import com.indo.common.utils.IPAddressUtil;
 import com.indo.game.pojo.dto.awc.AwcApiRequestParentData;
 import com.indo.game.service.awc.AwcCallbackService;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/awc")
@@ -29,12 +31,13 @@ public class AwcCallBackController {
     @AllowAccess
     public Object initGame(AwcApiRequestParentData awcApiRequestData, HttpServletRequest request) {
 
-
+        logger.info("开始时间 Date:{}", DateUtils.format(new Date(),DateUtils.newFormat));
         String ip = IPAddressUtil.getIpAddress(request);
         logger.info("awcCallBack  initGame 回调请求数据,IP:{} params:{}",ip,JSONObject.toJSONString(awcApiRequestData));
 
         Object object = awcAeSexybcrtCallbackService.awcCallback(awcApiRequestData,ip);
         logger.info("awcCallBack  initGame 回调返回数据, params:{}",object);
+        logger.info("结束时间 Date:{}", DateUtils.format(new Date(),DateUtils.newFormat));
         return object;
     }
 }
