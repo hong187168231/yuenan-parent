645db020edb0d1b42f7ff9f5bd6446d37ecdf95b
一休 <hongchengwei1986@gmail.com>
2022-11-11 16:30:41 +0800

    20221112卡密

---
+++ indo-admin/admin-boot/src/main/java/com/indo/admin/config/WebMvcConfig.java
@@ -0,0 +1,42 @@
+package com.indo.admin.config;
+
+import com.indo.game.intercept.AuthorizationInterceptor;
+import com.indo.game.intercept.UserTokenResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.List;
+import java.util.Locale;
+
+@Configuration
+public class WebMvcConfig extends com.indo.common.web.config.WebMvcConfig {
+
+    @Autowired
+    private AuthorizationInterceptor authorizationInterceptor;
+    @Autowired
+    private UserTokenResolver userTokenResolver;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userTokenResolver);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver ();
+        //指定默认语言为中文
+        localeResolver.setDefaultLocale(Locale.SIMPLIFIED_CHINESE);
+        return localeResolver;
+    }
+}
